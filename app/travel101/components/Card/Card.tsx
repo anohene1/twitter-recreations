c1a67f3c43045bbee10f85affc81d9a8fb0c6f24
Isaac Anohene <anoheneisaac@gmail.com>
2023-04-03 10:04:26 +0000

    fix build error again again

--- app/travel101/components/Card/Card.tsx
+++ app/travel101/components/Card/Card.tsx
@@ -1,45 +1,57 @@
-"use client"
+"use client";
 
-import React, {useEffect, useRef, useState} from 'react';
-import styles from './card.module.scss'
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./card.module.scss";
 import { gsap } from "gsap";
-import {Power4} from "gsap/gsap-core";
-
+import { Power4 } from "gsap/gsap-core";
 
 interface Props {
-    price: string,
-    city: string,
-    description: string,
-    videoUrl: string
+  price: string;
+  city: string;
+  description: string;
+  videoUrl: string;
 }
 
-function Card({price, city, description, videoUrl}: Props) {
+function Card({ price, city, description, videoUrl }: Props) {
+  let video = useRef(null);
+  let card = useRef(null);
+  const [isBeingHovered, setIsBeingHovered] = useState(false);
 
-    let video = useRef(null)
-    let card = useRef(null)
-    const [isBeingHovered, setIsBeingHovered] = useState(false)
+  useEffect(() => {
+    if (isBeingHovered) {
+      // @ts-ignore
+      video.current.play();
+      gsap.to(card.current, {
+        duration: 0.5,
+        css: { width: "44rem" },
+        ease: Power4.easeInOut,
+      });
+    } else {
+      // @ts-ignore
+      video.current.pause();
+      gsap.to(card.current, {
+        duration: 0.5,
+        css: { width: "22rem" },
+        ease: Power4.easeInOut,
+      });
+    }
+  });
 
-
-    useEffect(() => {
-        if (isBeingHovered) {
-            video.current.play()
-            gsap.to(card.current, {duration: 0.5, css: {width: '44rem'}, ease: Power4.easeInOut})
-        } else {
-            video.current.pause()
-            gsap.to(card.current, {duration: 0.5, css: {width: '22rem'}, ease: Power4.easeInOut})
-        }
-    })
-
-    return (
-        <div ref={card} onMouseEnter={() => setIsBeingHovered(true)} onMouseLeave={() => setIsBeingHovered(false)} className={styles.card}>
-            <div>
-                <h4>${price}</h4>
-                <h2>{city}</h2>
-                <p>{description}</p>
-            </div>
-            <video ref={video} muted loop src={videoUrl}></video>
-        </div>
-    );
+  return (
+    <div
+      ref={card}
+      onMouseEnter={() => setIsBeingHovered(true)}
+      onMouseLeave={() => setIsBeingHovered(false)}
+      className={styles.card}
+    >
+      <div>
+        <h4>${price}</h4>
+        <h2>{city}</h2>
+        <p>{description}</p>
+      </div>
+      <video ref={video} muted loop src={videoUrl}></video>
+    </div>
+  );
 }
 
 export default Card;
